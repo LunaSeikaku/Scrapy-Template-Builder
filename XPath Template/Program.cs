e534e04c94e2866ea4bca70f25e415262ce5c69b
Lunas Seikaku <icarus.fury@gmail.com>
2022-01-25 11:18:26 +0000

    general cleanup

--- XPath Template/Program.cs
+++ XPath Template/Program.cs
@@ -16,7 +16,7 @@ namespace XPath_Template
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Form());
         }
     }
 }
